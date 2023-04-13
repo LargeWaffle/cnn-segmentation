86bfcf925e53d1bbab4fec65bcfefe5aedf49d2e
Thomas Sirvent <sirventhomas@gmail.com>
2023-04-13 16:56:30 -0400

    better pytorch

---
+++ pytorch/imgutils.py
@@ -0,0 +1,92 @@
+import cv2
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+"""
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'deeplabv3_resnet101', pretrained=True)
+
+# create a color pallette, selecting a color for each class
+palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
+colors = torch.as_tensor([i for i in range(21)])[:, None] * palette
+colors = (colors % 255).numpy().astype("uint8")
+
+# plot the semantic segmentation predictions of 21 classes in each color
+r = Image.fromarray(output_predictions.byte().cpu().numpy()).resize(input_image.size)
+r.putpalette(colors)
+"""
+
+colormap = []
+for hexcode in mcolors.CSS4_COLORS.values():
+    r, g, b = [int(round(x * 255)) for x in mcolors.to_rgb(hexcode)]
+    colormap.append((r, g, b))
+
+label_colors = np.array([(0, 0, 0),  # 0=background
+                         # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
+                         (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128),
+                         # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
+                         (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0),
+                         # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
+                         (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
+                         # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
+                         (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
+
+
+# Define the helper function
+def decode_segmap(image, nc=21):
+    r = np.zeros_like(image).astype(np.uint8)
+    g = np.zeros_like(image).astype(np.uint8)
+    b = np.zeros_like(image).astype(np.uint8)
+
+    for l in range(0, nc):
+        idx = image == l
+        r[idx] = colormap[l][0]
+        g[idx] = colormap[l][1]
+        b[idx] = colormap[l][2]
+
+    rgb = np.stack([r, g, b], axis=2)
+    return rgb
+
+
+def image_overlay(image, segmented_image):
+    alpha = 1  # transparency for the original image
+    beta = 0.75  # transparency for the segmentation map
+    gamma = 0  # scalar added to each sum
+
+    image = np.array(image)
+    segmented_image = cv2.cvtColor(segmented_image, cv2.COLOR_RGB2BGR)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    cv2.addWeighted(image, alpha, segmented_image, beta, gamma, image)
+
+    return image
+
+
+def segment_map(output, img):
+    om = torch.argmax(output.squeeze(), dim=0).detach().cpu().numpy()
+
+    segmented_image = decode_segmap(om)
+
+    # Resize to original image size
+    segmented_image = cv2.resize(segmented_image, img.size, cv2.INTER_CUBIC)
+    overlayed_image = image_overlay(img, segmented_image)
+
+    # Plot
+    plt.figure(figsize=(12, 5), dpi=100)
+    plt.subplot(1, 3, 1)
+    plt.axis("off")
+    plt.title("Image")
+    plt.imshow(img)
+
+    plt.subplot(1, 3, 2)
+    plt.title("Segmentation")
+    plt.axis("off")
+    plt.imshow(segmented_image)
+
+    plt.subplot(1, 3, 3)
+    plt.title("Overlayed")
+    plt.axis("off")
+    plt.imshow(overlayed_image[:, :, ::-1])
+
+    plt.show()
--- pytorch/pytorch.py
+++ pytorch/pytorch.py
@@ -1,117 +1,52 @@
 import time
 
-import matplotlib.colors as mcolors
-import matplotlib.pyplot as plt
-import numpy as np
-import torch
 import torch.utils.data
 import torchvision.datasets as dset
-# Apply the transformations needed
-import torchvision.transforms as T
+
+from imgutils import segment_map
+
 from torchvision import models
-from torchvision.models.segmentation import FCN_ResNet101_Weights, DeepLabV3_ResNet101_Weights
+from torchvision.models.segmentation import FCN_ResNet101_Weights, DeepLabV3_ResNet101_Weights, \
+    DeepLabV3_MobileNet_V3_Large_Weights
 
-img_size = 640
 
-fcn = models.segmentation.fcn_resnet101(weights=FCN_ResNet101_Weights.COCO_WITH_VOC_LABELS_V1).eval()
-dlab = models.segmentation.deeplabv3_resnet101(weights=DeepLabV3_ResNet101_Weights.COCO_WITH_VOC_LABELS_V1).eval()
+def load_model(choice="dlab"):
+    if choice == "dlab":
+        w = DeepLabV3_ResNet101_Weights.COCO_WITH_VOC_LABELS_V1
+        m = models.segmentation.deeplabv3_resnet101(weights=w)
 
-"""
-# model = torch.hub.load('pytorch/vision:v0.10.0', 'deeplabv3_resnet101', pretrained=True)
+    elif choice == "dlab_large":
+        w = DeepLabV3_MobileNet_V3_Large_Weights.COCO_WITH_VOC_LABELS_V1
+        m = models.segmentation.deeplabv3_mobilenet_v3_large(weights=w)
+    else:
+        w = FCN_ResNet101_Weights.COCO_WITH_VOC_LABELS_V1
+        m = models.segmentation.fcn_resnet101(weights=w)
 
-# create a color pallette, selecting a color for each class
-palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
-colors = torch.as_tensor([i for i in range(21)])[:, None] * palette
-colors = (colors % 255).numpy().astype("uint8")
+    return m.eval(), w
 
-# plot the semantic segmentation predictions of 21 classes in each color
-r = Image.fromarray(output_predictions.byte().cpu().numpy()).resize(input_image.size)
-r.putpalette(colors)
-"""
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print("Running on", device)
+print("Segmentation project running on", device)
 
-colormap = []
-for hexcode in mcolors.CSS4_COLORS.values():
-    r, g, b = [int(round(x * 255)) for x in mcolors.to_rgb(hexcode)]
-    colormap.append((r, g, b))
-
-label_colors = np.array([(0, 0, 0),  # 0=background
-                         # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
-                         (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128),
-                         # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
-                         (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0),
-                         # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
-                         (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
-                         # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
-                         (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
-
-
-# Define the helper function
-def decode_segmap(image, nc=21):
-    r = np.zeros_like(image).astype(np.uint8)
-    g = np.zeros_like(image).astype(np.uint8)
-    b = np.zeros_like(image).astype(np.uint8)
-
-    for l in range(0, nc):
-        idx = image == l
-        r[idx] = colormap[l][0]
-        g[idx] = colormap[l][1]
-        b[idx] = colormap[l][2]
-
-    rgb = np.stack([r, g, b], axis=2)
-    return rgb
-
-
-transformations = T.Compose([
-    T.Resize(img_size),
-    T.CenterCrop(img_size),
-    T.ToTensor(),
-    T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-
-
-def segment(net, img, dev):
-    inp = transformations(img).unsqueeze(0).to(dev)
-
-    out = net.to(dev)(inp)['out']
-
-    om = torch.argmax(out.squeeze(), dim=0).detach().cpu().numpy()
-
-    rgb = decode_segmap(om)
-
-    rs = T.Compose([
-        T.ToPILImage(),
-        T.Resize(size=(img.size[::-1]))
-    ])
-
-    result = rs(rgb)
-
-    f = plt.figure(figsize=(10, 5))
-    plt.subplot(1, 2, 1)
-    plt.imshow(img)
-    plt.axis('off')
-
-    plt.subplot(1, 2, 2)
-    plt.imshow(result)
-    plt.axis('off')
-    plt.show()
-
-
-test_imgs = dset.ImageFolder(root="data/images/folder/")
+test_imgs = dset.ImageFolder(root="../data/images/folder/")
 
 dataloader = torch.utils.data.DataLoader(test_imgs, batch_size=None, shuffle=True)
 
+model, w = load_model()
+transformations = w.transforms()
+
 for i, (data, _) in enumerate(dataloader):
     print("Iteration %d" % i)
+    inp = transformations(data).unsqueeze(0).to(device)
+
+    st = time.time()
     with torch.no_grad():
+        out = model.to(device)(inp)['out']
+    end = time.time()
 
-        st = time.time()
-        segment(dlab, data, device)
-        end = time.time()
+    print(f"Inference took: {end - st:.2f}", )
 
-        print(f"Inference took: {end - st:.2f}", )
+    segment_map(out, data)
 
-        if i == 5:
-            break
+    if i == 5:
+        break
