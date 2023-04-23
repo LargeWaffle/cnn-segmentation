f68c5f350668368abb2f13b3772ce6cef77d4034
Thomas Sirvent <sirventhomas@gmail.com>
2023-04-23 19:55:05 -0400

    switch to tkinter

--- gui/main.py
+++ gui/main.py
@@ -1,79 +1,67 @@
-import dearpygui.dearpygui as dpg
-from PIL import Image
-from pytorch import run_segmentation
-import numpy as np
+from tkinter import *
+from tkinter import filedialog
+from PIL import Image, ImageTk
 
-W_WIDTH = 1280
-W_HEIGHT = 800
+APPW = 1400
+APPY = 600
+GRIDPADX = 15
+GRIDPADY = 15
+IMGSIZE = (350, 350)
 
-IMG_W = int(W_WIDTH * 0.8)
-SIDE_W = int(W_WIDTH * 0.2)
+window = Tk()
+window.title("Image segmentation")
 
-POP_W = int(W_WIDTH // 1.3)
-POP_H = int(W_HEIGHT // 1.3)
+window.geometry(f'{APPW}x{APPY}+0+0')
+window.resizable(False, False)
 
-dpg.create_context()
+window.config(bg="lightgrey")
 
+titleframe = Frame(window, bg="lightgrey")
+buttonframe = Frame(window, bg="lightgrey")
+photoframe = Frame(window, bg="lightgrey")
+classframe = Frame(window, bg="lightgrey")
 
-def add_and_load_image(image, tag, parent=None):
-    w, h = image.size
-
-    image.putalpha(255)
-    dpg_image = np.frombuffer(image.tobytes(), dtype=np.uint8) / 255.0
-
-    with dpg.texture_registry() as reg_id:
-        texture_id = dpg.add_static_texture(w, h, dpg_image, parent=reg_id)
-
-    if parent:
-        return dpg.add_image(texture_id, tag=tag, parent=parent)
-    else:
-        return dpg.add_image(texture_id, tag=tag)
-
-
-def load_imgs(_, app_data):
-    with dpg.value_registry():
-        dpg.add_string_value(default_value=app_data['file_path_name'], tag="base_path")
-
-    img = Image.open(dpg.get_value("base_path"))
-    res_img = run_segmentation(img)
-
-    add_and_load_image(image=img, tag="base_img", parent=image_window)
-    add_and_load_image(image=res_img, tag="res_img", parent=image_window)
+titleframe.grid(row=0, column=0, columnspan=4, sticky='n')
+buttonframe.grid(row=1, column=0, columnspan=4, sticky='n')
+photoframe.grid(row=2, column=0, columnspan=4, sticky='n')
+classframe.grid(row=3, column=0, columnspan=4, sticky='n')
 
 
 def rerun():
-    dpg.delete_item("res_img")
-
-    img = Image.open(dpg.get_value("base_path"))
-    res_img = run_segmentation(img)
-
-    add_and_load_image(image=res_img, tag="res_img", parent=image_window)
-
-
-def cancel_callback():
     pass
 
 
-with dpg.file_dialog(directory_selector=False, show=False, id="filepicker", callback=load_imgs,
-                     cancel_callback=cancel_callback,
-                     width=POP_W, height=POP_H):
-    dpg.add_file_extension("Images (*.png *.jpg *.jpeg){.png,.jpg,.jpeg}", color=(255, 255, 255, 255))
+def select_file():
+    file = filedialog.askopenfilename(title="Select an image", filetypes=[("Image file", "*.jpg *.jpeg *.png")])
+    image = Image.open(file)
+    image.thumbnail(IMGSIZE, Image.LANCZOS)
 
-with dpg.window(width=W_WIDTH, height=W_HEIGHT, autosize=True) as main_window:
+    photo = ImageTk.PhotoImage(image)
 
-    with dpg.child_window(pos=[0, 0], width=IMG_W, autosize_x=True, autosize_y=True, parent=main_window)as image_window:
-        dpg.add_text("Segmentation results", tag="img_res_title")
-        dpg.add_spacer(height=15)
+    lb1 = Label(photoframe, image=photo)
+    lb1.photo = photo
+    lb1.grid(row=1, column=0, padx=GRIDPADX, pady=GRIDPADY)
 
-    with dpg.child_window(pos=[IMG_W, 0], width=SIDE_W, autosize_x=True, autosize_y=True, parent=main_window):
-        dpg.add_button(label="Choose Image", tag="file_button", callback=lambda: dpg.show_item("filepicker"), width=-1)
-        dpg.add_spacer(height=5)
-        dpg.add_button(label="Regenerate", tag="seg_button", callback=rerun, width=-1)
+    lb2 = Label(photoframe, image=photo)
+    lb2.photo = photo
+    lb2.grid(row=1, column=1, padx=GRIDPADX, pady=GRIDPADY)
 
-dpg.create_viewport(title='Image segmentation', width=W_WIDTH, height=W_HEIGHT, x_pos=0, y_pos=0)
-dpg.setup_dearpygui()
-dpg.show_viewport()
-dpg.set_primary_window(window=main_window, value=True)
-dpg.start_dearpygui()
+    lb3 = Label(photoframe, image=photo)
+    lb3.photo = photo
+    lb3.grid(row=1, column=2, padx=GRIDPADX, pady=GRIDPADY)
 
-dpg.destroy_context()
+    lbc = Label(classframe, text="yo")
+    lbc.grid(row=0, column=0, padx=GRIDPADX, pady=GRIDPADY)
+
+
+maintitle = Label(titleframe, text="Semantic segmentation", bg="lightgrey", font=("Arial Bold", 25))
+filebtn = Button(buttonframe, text="Choose image", command=select_file, padx=10, pady=10)
+regenbtn = Button(buttonframe, text="Regenerate", command=rerun, padx=10, pady=10)
+quitbtn = Button(buttonframe, text="Quit", command=window.destroy, padx=10, pady=10)
+
+maintitle.grid(row=0, column=0, padx=GRIDPADX, pady=GRIDPADY)
+filebtn.grid(row=0, column=1, padx=GRIDPADX, pady=GRIDPADY)
+regenbtn.grid(row=0, column=2, padx=GRIDPADX, pady=GRIDPADY)
+quitbtn.grid(row=0, column=3, padx=GRIDPADX, pady=GRIDPADY)
+
+window.mainloop()
